4cfb37a0bbc605bf32e52792a6820ecefec73b54
Renanito69 <renanbascristian@gmail.com>
2026-06-19 19:17:36 -0300

    baixar as depedenciar

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 from watcher import iniciar_monitoramento
 from config import PASTA_JSON
+from depedencias import instalar_dependencias
 
-
-if __name__ == "__main__":
-    os.makedirs(PASTA_JSON, exist_ok=True)
-    iniciar_monitoramento(PASTA_JSON)
-    
+try:
+    if __name__ == "__main__":
+        os.makedirs(PASTA_JSON, exist_ok=True)
+        iniciar_monitoramento(PASTA_JSON)
+except Exception as e:
+    instalar_dependencias()
+    print(f"Dependências instaladas. Por favor, reinicie o programa.")
